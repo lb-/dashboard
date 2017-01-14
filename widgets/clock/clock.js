e3cad6f6ad6cf24bc2943c2cdf0f5111e4758821
LB <mail@lb.ee>
2017-01-14 16:08:53 +0700

    start work on clock

--- widgets/clock/clock.js
+++ widgets/clock/clock.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import moment from 'moment';
+// import 'moment-timezone';
 import Widget from '../../assets/javascripts/widget';
 
 import './clock.scss';
@@ -11,6 +13,7 @@ Widget.mount(class Clock extends Widget {
   }
   update() { this.setState(Clock.dateTime()); }
   render() {
+
     return (
       <div className={this.props.className}>
         <h1 className="date">{this.state.date}</h1>
@@ -20,13 +23,13 @@ Widget.mount(class Clock extends Widget {
   }
   static formatTime(i) { return i < 10 ? "0" + i : i; }
   static dateTime() {
-    var today = new Date(),
+    var today = new Date(),//moment().tz("America/Toronto").toDate(),
         h = today.getHours(),
         m = today.getMinutes(),
         s = today.getSeconds(),
         m = Clock.formatTime(m),
         s = Clock.formatTime(s);
-
+    console.log(moment()); //next step - make this use timezone
     return {
       time: (h + ":" + m + ":" + s),
       date: today.toDateString(),
